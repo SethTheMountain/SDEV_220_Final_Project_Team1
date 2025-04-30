8ca0deeb4908672238b7e4a7f7a94e29876dbe26
SethTheMountain <smsorrell27@gmail.com>
2025-04-30 19:03:34 -0400

    fixed settings.py

--- menards_register/settings.py
+++ menards_register/settings.py
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = 'your-secret-key'
-DEBUG = True
-ALLOWED_HOSTS = []
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'registration',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'registration.middleware.TimezoneMiddleware',
-]
-
-ROOT_URLCONF = 'menards_register.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-=======
 """
 Django settings for menards_register project.
 
@@ -108,40 +63,11 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
->>>>>>> 5bac7396538c333b6b061a6a48f0cb071cbf1da0
             ],
         },
     },
 ]
 
-<<<<<<< HEAD
-WSGI_APPLICATION = 'menards_register.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
-
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = []
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-=======
 WSGI_APPLICATION = "menards_register.wsgi.application"
 
 
@@ -205,4 +131,3 @@ STATICFILES_DIRS = [BASE_DIR / "registration/static"]
 
 # Add this line
 STATIC_ROOT = BASE_DIR / "staticfiles"
->>>>>>> 5bac7396538c333b6b061a6a48f0cb071cbf1da0
